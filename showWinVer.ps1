9362d910233f333fefba30d470eb3ca386805880
darcy <info@darcy-it.com>
2023-01-13 22:54:26 +0900

    ver update.

--- showWinVer.ps1
+++ showWinVer.ps1
@@ -2,6 +2,10 @@
 # file: showWinVer.ps1
 # usage: .\showWinVer.ps1
 #
+# Description: Show Windows Version
+# url: https://atmarkit.itmedia.co.jp/ait/articles/1707/31/news028.html
+# url: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-computerinfo?view=powershell-7.1
+#
 
 [System.Environment]::OSVersion.Version
 
@@ -9,10 +13,6 @@ Get-ComputerInfo | select WindowsProductName, WindowsVersion, OsHardwareAbstract
 
 (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ReleaseId
 
-# (Get-WmiObject -class Win32_OperatingSystem).Caption
-# (Get-WmiObject -class Win32_OperatingSystem).Caption | select -first 1
-
 systeminfo /fo csv | ConvertFrom-Csv | select OS*, System*, Hotfix* | Format-List
-
 
 winver
