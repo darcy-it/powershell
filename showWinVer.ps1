1d139005265c5aa95de5ad24c5bc16ba89d8e558
darcy <info@darcy-it.com>
2023-01-13 20:40:04 +0900

    win version add.

---
+++ showWinVer.ps1
@@ -0,0 +1,15 @@
+#
+# file: showWinVer.ps1
+# usage: .\showWinVer.ps1
+#
+
+[System.Environment]::OSVersion.Version
+
+Get-ComputerInfo | select WindowsProductName, WindowsVersion, OsHardwareAbstractionLayer
+
+(Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ReleaseId
+
+# (Get-WmiObject -class Win32_OperatingSystem).Caption
+# (Get-WmiObject -class Win32_OperatingSystem).Caption | select -first 1
+
+systeminfo /fo csv | ConvertFrom-Csv | select OS*, System*, Hotfix* | Format-List
